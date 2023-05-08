68e4a728598b8dbbcdd653009adf9ac73a0e0ac6
Snaky <snakypeaky3@gmail.com>
2023-05-08 08:48:22 +0300

    Аннотации. Mypy moment (нет)

--- cogs/moderation/mute.py
+++ cogs/moderation/mute.py
@@ -1,24 +1,29 @@
-import disnake
+from disnake import ApplicationCommandInteraction, Member
 from disnake.ext import commands
 
+
 class Mute(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    async def cog_load(self):
+    async def cog_load(self) -> None:
         print(f"[COG] {self.__cog_name__} was loaded.")
 
-    @commands.slash_command(name="mute",
-                            description="Мьютит участника.",
-                            dm_permission=False)
-    async def mute(self, ctx,
-                   member: disnake.Member,
-                   time: int,
-                   reason = "не указано."):
-        
+    @commands.slash_command(
+        name="mute",
+        description="Мьютит участника.",
+        dm_permission=False
+    )
+    async def mute(
+        self, ctx: ApplicationCommandInteraction,
+        member: Member,
+        time: int,
+        reason: str = "не указано."
+    ) -> None:
         await member.timeout(duration=time, reason=reason)
-        await ctx.send(f"Участник {member.mention} получил мьют на {time} секунд, по причине: {reason}")
+        await ctx.send(
+            f"Участник {member.mention} получил мьют на {time} секунд, по причине: {reason}")
 
 
-def setup(bot):
+def setup(bot: commands.Bot) -> None:
     bot.add_cog(Mute(bot))
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
+import datetime
+import os
+
 import disnake
 from disnake.ext import commands
 
-import os
-import datetime
-
 from tokens import tokens
 
-
-bot = commands.Bot(command_prefix = commands.when_mentioned_or("!"),
-                   intents = disnake.Intents.all(),
-                   activity = disnake.Game("drewsupport.github.io"),
-                   status = disnake.Status.idle)
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    intents=disnake.Intents.all(),
+    activity=disnake.Game("drewsupport.github.io"),
+    status=disnake.Status.idle
+)
 
 bot.remove_command('help')
+
 
 # TEST
 @bot.command()
 async def host(ctx):
-    await ctx.send(f"Current host time: {datetime.datetime.now().ctime()}\n"
-                   f"Start host time: {startTime}")
+    await ctx.send(
+        f"Current host time: {datetime.datetime.now().ctime()}\n"
+        f"Start host time: {startTime}"
+    )
+
 
 # RUN
 @bot.event
 async def on_ready():
-
-    print(f"Loggined as {bot.user.name}#{bot.user.discriminator} (USER ID: {bot.user.id})\n"
+    print(f"Logged as {bot.user} (USER ID: {bot.user.id})\n"
           f"Start time: {startTime}")
 
-for category in os.listdir("./cogs"):
-    for file in os.listdir(f"./cogs/{category}"):
-        if file.endswith(".py"):
-            bot.load_extension(f"cogs.{category}." + file[:-3])
 
+for category in os.listdir("cogs"):
+    for file in os.listdir(f"cogs/{category}"):
+        if file.endswith(".py"):
+            bot.load_extension(f"cogs.{category}.{file[:-3]}")
 
 startTime = datetime.datetime.now().ctime()
 bot.run(tokens['debug'])
