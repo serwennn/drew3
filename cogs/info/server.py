23b8f373ed16eb17bb8aaa0a48f7b4fc6f78ecdc
Kozhakhmetuly Daniyaru <nassen.d230@gmail.com>
2023-05-29 00:47:20 +0600

    'server' command is finally done

--- cogs/info/server.py
+++ cogs/info/server.py
@@ -22,7 +22,8 @@ class Server(commands.Cog):
     ) -> None:
         
         embed = Embed(
-            title = f"🌸・О **{ctx.guild.name}**",
+            title = f"🌸・Статистика **{ ctx.guild.name }**",
+            description = f"Владелец: { ctx.guild.owner.mention } ({ ctx.guild.owner })\nСоздан: <t:{ int(ctx.guild.created_at.timestamp()) }>",
             color = configs['color']
         )
         if ctx.guild.icon != None: embed.set_thumbnail(ctx.guild.icon)
@@ -31,6 +32,13 @@ class Server(commands.Cog):
         for member in ctx.guild.members:
             if member.bot == True: member_bot_count += 1
 
+        ver_level_raw = ctx.guild.verification_level.name
+        if ver_level_raw == "none": ver_level = "Отсутствует (Кто-угодно может зайти на сервер)"
+        elif ver_level_raw == "low": ver_level = "Низкий (Участнику необходимо иметь подтвержденный Email)"
+        elif ver_level_raw == "medium": ver_level = "Средний (Подтвержденный Email и 5 минут с регистрации аккаунта)"
+        elif ver_level_raw == "high": ver_level = "Высокий (Должен быть участником этого сервера больше 10 минут)"
+        elif ver_level_raw == "highest": ver_level = "Самый высокий (Участник должен иметь подтвержденный номер телефона)"
+
         embed.add_field(
             name = "👤・Участники:",
             value = f"Всего: { ctx.guild.member_count }\nЛюдей: { ctx.guild.member_count - member_bot_count }\nБотов: { member_bot_count }",
@@ -38,11 +46,18 @@ class Server(commands.Cog):
         )
         embed.add_field(
             name = "☕・Каналы:",
-            value = f"sdansdauinfue"
+            value = f"Всего: { len(ctx.guild.channels) }\nТекстовые: { len(ctx.guild.text_channels) }\nГолосовые: { len(ctx.guild.voice_channels) }",
+            inline = True
         )
         embed.add_field(
-            name = "・Прочее:",
-            value = f"smfoiesfoimesois"
+            name = "✨・Прочее:",
+            value = f"Всего категорий: { len(ctx.guild.categories) }\nРолей: { len(ctx.guild.roles) }\nЭмодзи: { len(ctx.guild.emojis) }",
+            inline = True
+        )
+        embed.add_field(
+            name = "🛡️・Уровень верификации:",
+            value = ver_level,
+            inline = False
         )
 
         await ctx.send(embed = embed)
